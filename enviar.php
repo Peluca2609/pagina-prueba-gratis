0bc1a434949959bfe80182644bc70db92169754a
Peluca2609 <65892410+Peluca2609@users.noreply.github.com>
2020-05-25 06:11:25 -0500

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,23 +1,16 @@
 <?php
-// Llamando a los campos
-$nombre = $_POST['Nombre'];
+$Nombre = $_POST['Nombre'];
 $dni = $_POST['dni'];
 $edad = $_POST['edad'];
 $peso = $_POST['peso'];
 $talla = $_POST['talla'];
-
-// Datos para el correo
 $destinatario = "javiergalvezsordones@gmail.com";
 $asunto = "DC:Tamizaje por COVID-19";
-
 $carta = "De: $Nombre \n";
 $carta .= "DNI: $dni \n";
 $carta .= "Edad: $edad \n";
 $carta .= "Su peso es: $peso \n";
 $carta .= "Su talla es: $talla";
-
-// Enviando Mensaje
 mail($destinatario, $asunto, $carta);
 header('Location:mensaje.html');
-
 ?>
